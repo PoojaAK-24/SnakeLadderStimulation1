6ec0706f8bdf22709408b50e200660d5e7629700
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-07-05 16:01:13 +0530

    Player Starting At Position Zero

---
+++ UC1_PlayerStartingPositionZero.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLadderStimulation
+{
+    class UC1_PlayerStartingPositionZero
+    {
+        public static void snake()
+        { 
+            int position = 0;
+            Console.WriteLine("Player is at position : " + position);
+        }
+    }
+}
