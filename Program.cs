d0dfcd79556d4db07e64bff7ca5dc7a2b6c1b560
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-07-05 15:59:47 +0530

    Welcome to Snake Ladder

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SnakeLadderStimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Snake Ladder Game");
+            //UC1_PlayerStartingPositionZero.snake();
+            //UC2_PlayerRollsDice.snake1();
+            // UC3_PlayerChecksForOption.snake2();
+            // UC4_PlayerWinningPosition.snake3();
+            // UC5PlayerReachesExactWinningPosition.snake4();
+            // UC6_NumberOfDicePlayed.snake5();
+           // UC7_PlayingWithTwoPlayers.snake6();
+        }
+    }
+}
